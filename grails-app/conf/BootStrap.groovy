2b75b105095a934ee8a59d2ec102f8c13666d48c
techgaun <coolsamar207@gmail.com>
2013-08-23 20:06:02 +0545

    Updated bootstrap with UserRole information

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,6 @@
 import com.dss.RequestMap
 import com.dss.User
+import com.dss.*
 
 class BootStrap {
 
@@ -13,8 +14,9 @@ class BootStrap {
         new RequestMap(url: '/user/**', configAttribute: 'IS_AUTHENTICATED_ANONYMOUSLY').save(flush:true)
         new RequestMap(url: '/admin/**', configAttribute: 'ROLE_ADMIN').save(flush:true)
         new RequestMap(url: '/*/**', configAttribute: 'ROLE_ADMIN, ROLE_USER').save(flush:true)
-        //new User(user_id: '1', username: 'admin', password: 'admin').save(flush: true)
-
+        new User(user_id: '1', username: 'admin', password: 'admin', enabled: true, accountExpired: false, accountLocked: false, passwordExpired: false).save(flush: true)
+        new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        new UserRole(user: User.findById(1),role: Role.findByAuthority("ROLE_ADMIN")).save(flush: true)
     }
     def destroy = {
     }
